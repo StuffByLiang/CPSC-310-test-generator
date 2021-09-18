056ec0eaeb526e64b5be99514753155c03c07987
Liang Liu <stuffbyliang@gmail.com>
2021-09-18 10:11:41 -0700

    Add result too large error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import { Box, Button, Container, Grid, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
 import axios from "axios";
 import { useState } from "react";
 import AceEditor from "react-ace";
@@ -8,7 +15,15 @@ import "ace-builds/src-noconflict/theme-github";
 import { downloadFile } from "./utils/downloadFile";
 
 const API_URL = "https://cs310.students.cs.ubc.ca/ui/query";
-const DEFAULT_VALUE = JSON.stringify(JSON.parse(`{"WHERE":{"OR":[{"AND":[{"GT":{"courses_avg":90}},{"IS":{"courses_dept":"adhe"}}]},{"EQ":{"courses_avg":95}}]},"OPTIONS":{"COLUMNS":["courses_dept","courses_id","courses_avg"],"ORDER":"courses_avg"}}`), null, '\t')
+const DEFAULT_VALUE = JSON.stringify(
+  JSON.parse(
+    `{"WHERE":{"OR":[{"AND":[{"GT":{"courses_avg":90}},{"IS":{"courses_dept":"adhe"}}]},{"EQ":{"courses_avg":95}}]},"OPTIONS":{"COLUMNS":["courses_dept","courses_id","courses_avg"],"ORDER":"courses_avg"}}`
+  ),
+  null,
+  "\t"
+);
+const RESULT_TOO_LARGE_ERROR_MSG =
+  "The result is too big. Only queries with a maximum of 5000 results are supported.";
 
 function App() {
   const [value, setValue] = useState(DEFAULT_VALUE);
@@ -34,8 +49,8 @@ function App() {
         title,
         input: query,
         errorExpected: false,
-        with: result
-      }
+        with: result,
+      };
       response = JSON.stringify(file, null, 2);
       setError(false);
     } catch (error) {
@@ -44,13 +59,18 @@ function App() {
         errorMsg = (error as any).response.data.error;
       }
 
+      console.log(errorMsg);
+
       const file = {
         errorMsg,
         title,
         input: JSON.parse(value),
         errorExpected: true,
-        with: "InsightError"
-      }
+        with:
+          errorMsg === RESULT_TOO_LARGE_ERROR_MSG
+            ? "ResultTooLargeError"
+            : "InsightError",
+      };
 
       response = JSON.stringify(file, null, 2);
       setError(true);
@@ -58,56 +78,66 @@ function App() {
       setResult(response);
     }
     return response;
-  }
+  };
 
   return (
-      <Container>
-        <Typography variant="h2">CS310 Test Generator </Typography>
-        <Grid container spacing={4}>
-          <Grid item xs={6}>
-            <Box mb={1}>
-              <TextField size="small" fullWidth value={title} variant="outlined" label="Test Case Title" onChange={(e) => setTitle(e.target.value)} />
-            </Box>
-            <AceEditor
-              width="100%"
-              mode="json"
-              theme="github"
-              onChange={setValue}
-              value={value}
-              name="id"
-              editorProps={{ $blockScrolling: true }}
-              setOptions={{
-                enableBasicAutocompletion: true,
-                enableLiveAutocompletion: true,
-                enableSnippets: true
-              }}
+    <Container>
+      <Typography variant="h2">CS310 Test Generator </Typography>
+      <Grid container spacing={4}>
+        <Grid item xs={6}>
+          <Box mb={1}>
+            <TextField
+              size="small"
+              fullWidth
+              value={title}
+              variant="outlined"
+              label="Test Case Title"
+              onChange={(e) => setTitle(e.target.value)}
             />
-            <Box my={2} display="flex" justifyContent="flex-end">
-              <Box mr={1}>
-                <Button variant="contained" onClick={() => fetchData()}>
-                  Submit
-                </Button>
-              </Box>
-              <Button variant="contained" onClick={async() => {
-                const response = await fetchData()
-                downloadFile(response, title);
-              }}>
-                Download File
+          </Box>
+          <AceEditor
+            width="100%"
+            mode="json"
+            theme="github"
+            onChange={setValue}
+            value={value}
+            name="id"
+            editorProps={{ $blockScrolling: true }}
+            setOptions={{
+              enableBasicAutocompletion: true,
+              enableLiveAutocompletion: true,
+              enableSnippets: true,
+            }}
+          />
+          <Box my={2} display="flex" justifyContent="flex-end">
+            <Box mr={1}>
+              <Button variant="contained" onClick={() => fetchData()}>
+                Submit
               </Button>
             </Box>
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              label="Test File"
-              multiline
-              fullWidth
-              rows={25}
-              value={result}
-              error={error}
-            />
-          </Grid>
+            <Button
+              variant="contained"
+              onClick={async () => {
+                const response = await fetchData();
+                downloadFile(response, title);
+              }}
+            >
+              Download File
+            </Button>
+          </Box>
         </Grid>
-      </Container>
+        <Grid item xs={6}>
+          <TextField
+            label="Test File"
+            multiline
+            fullWidth
+            rows={25}
+            value={result}
+            error={error}
+          />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
